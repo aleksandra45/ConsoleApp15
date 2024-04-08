40cf86202cf42ab80e4e5fd642d72f818d0b88ed
aleksandravasileva6 <aleksandravasileva375@gmail.com>
2024-04-08 19:39:05 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace Task1;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Укажите путь до каталога:");
+        var path = Console.ReadLine();
+        GetCatalogs(path!);
+    }
+    static void GetCatalogs(string path)
+    {
+        if (Directory.Exists(path))
+        {
+            try
+            {
+                var dirTime = Directory.GetLastWriteTime(path);
+                Console.WriteLine($"Дата, время создание папки {dirTime}");
+                var timeSpan = TimeSpan.FromMinutes(30);
+                var dateTime = DateTime.Now - timeSpan;
+                if (dateTime <= dirTime) return;
+                var dirs = Directory.GetDirectories(path);
+                foreach (var dir in dirs)
+                {
+                    try
+                    {
+                        Directory.Delete(dir, true);
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine($"Для директории {dir} установлен статус {e.Message}");
+                        throw;
+                    }
+                    finally
+                    {
+                        Console.WriteLine($"Удаление поддиректорий по пути {path} произведена");
+                    }
+                }
+
+                var files = Directory.GetFiles(path);
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Для файла {file} установлен статус {e.Message}");
+                        throw;
+                    }
+                    finally
+                    {
+                        Console.WriteLine($"Удаление файлов по пути {path} произведена");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Директория не существует");
+        }
+
+        Console.ReadKey();
+    }
+}
